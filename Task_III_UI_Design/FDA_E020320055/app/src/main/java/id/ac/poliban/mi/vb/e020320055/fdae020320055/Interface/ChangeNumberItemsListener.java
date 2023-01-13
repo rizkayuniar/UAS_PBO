37a4aa689a60fe3d964bc025f575e073d746077b
Rizka Yuniar Fahmi <rizkafahmi60@gmail.com>
2023-01-13 11:47:47 +0800

    E020320055_VB MI

---
+++ Task_III_UI_Design/FDA_E020320055/app/src/main/java/id/ac/poliban/mi/vb/e020320055/fdae020320055/Interface/ChangeNumberItemsListener.java
@@ -0,0 +1,5 @@
+package id.ac.poliban.mi.vb.e020320055.fdae020320055.Interface;
+
+public interface ChangeNumberItemsListener {
+    void changed();
+}
---
+++ Task_I_UI_Design/Project82_E020320055/app/src/main/java/id/ac/poliban/mi/vb/e020320055/project82e020320055/MainActivity.java
@@ -0,0 +1,15 @@
+package id.ac.poliban.mi.vb.e020320055.project82e020320055;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        setTitle("Rizka - Project 82 E020320055");
+    }
+}
